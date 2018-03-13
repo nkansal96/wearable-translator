dc9190191d5e1a29a3f6f41b1ebcc0e60a7a06fb
smaranki <33140129+smaranki@users.noreply.github.com>
2018-03-13 16:46:20 -0700

    Automatic dependency installation for Linux and OSX

---
+++ execute_linux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pip2 install --upgrade watson-developer-cloud
+pip2 install --playsound
+pip2 install -U PyObjc
+pip2 install --upgrade google-cloud
+
+sudo apt-get install python-pyaudio
+pip2 install auroraapi==0.0.10
+
+python2 translator.py $1 $2 $3
