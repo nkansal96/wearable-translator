8ee8dd481fa0a5896f65ccc2387aed41b9524efa
Jason Alvarez-Cohen <jason11ac@gmail.com>
2018-03-16 13:05:44 -0700

    Added sudo

--- execute_mac.sh
+++ execute_mac.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-pip2 install --upgrade watson-developer-cloud
-pip2 install --playsound
-pip2 install -U PyObjc
-pip2 install --upgrade google-cloud
+sudo pip2 install --upgrade watson-developer-cloud
+sudo pip2 install --playsound
+sudo pip2 install -U PyObjc
+sudo pip2 install --upgrade google-cloud
 
 brew install portaudio
-pip2 install auroraapi==0.0.10
+sudo pip2 install auroraapi==0.0.10
 
 python2 translator.py $1 $2 $3
